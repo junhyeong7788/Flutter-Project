dca77970d725f2066dfc1f5eefefd2d21b909bae
junhyeong7788 <ryker77@naver.com>
2023-11-13 17:27:52 +0900

    'stat변수지정 : title, bottom, MaterialApp/ FloatingActionButton 추가, contact image add, ListView add'

--- assignment/market/lib/main.dart
+++ assignment/market/lib/main.dart
@@ -1,27 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Assignment1());
+  runApp(const Assignment2());
 }
-class Assignment1 extends StatelessWidget {
-  const Assignment1({super.key});
 
+var title = AppBar(title: Text('Assignment 1'));
+var bottom = BottomAppBar(
+  height: 50, //하단앱바 높이조절
+  child: Row( //가로, column: 세로
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly, //아이콘가로정렬
+    children: const [
+      Icon(Icons.phone),
+      Icon(Icons.message),
+      Icon(Icons.contact_page),
+    ],
+  ),
+);
+
+
+class Assignment2 extends StatefulWidget {
+  const Assignment2({super.key});
+
+  @override
+  State<Assignment2> createState() => _Assignment2State();
+}
+
+class _Assignment2State extends State<Assignment2> {
+
+  var a = 1;
+  var name = ['김영숙', '홍길동', '피자집'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: Text('Assignment 1')),
-          bottomNavigationBar: BottomAppBar(
-            height: 50, //하단앱바 높이조절
-            child: Row( //가로, column: 세로
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly, //아이콘가로정렬
-              children: const [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-              ),
+          floatingActionButton: FloatingActionButton(
+            child: Text('Add'),
+            onPressed: (){},
           ),
+          appBar: title,
+          body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (c, i){
+              return ListTile(
+                leading: Image.asset('profile.png'),
+                title: Text(name[i]),
+              );
+            },
+          ),
+          bottomNavigationBar: bottom,
         ),
     );
   }
