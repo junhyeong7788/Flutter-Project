86ab0a68143111a17b7be1b6130a7e8251c0fc4f
junhyeong7788 <ryker77@naver.com>
2023-11-18 04:51:39 +0900

    '간단한 연락처 만들기 완료'

--- assignment/reset/lib/main.dart
+++ assignment/reset/lib/main.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp2());
 }
+
 class MyApp2 extends StatelessWidget {
   const MyApp2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('HelloWorld!'),)
+    return MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(
+          leading: Text('My App2'), titleTextStyle: ,
+
+      )
+      )
     );
   }
 }
